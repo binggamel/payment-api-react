464a7ef3dbb77c3711db11d347aaf9c35f95c3c7
binggamel <kcyeun1203@naver.com>
2021-09-06 13:51:41 +0900

    first commit

---
+++ src/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Card, Button } from "antd";
+import "antd/dist/antd.css";
+
+const { Meta } = Card;
+
+function header() {
+  return (
+    <>
+      <Button>Default Button</Button>
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={
+          <img
+            alt="example"
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          />
+        }
+      >
+        <Meta title="Europe Street beat" description="www.instagram.com" />
+      </Card>
+      <Card
+        title="Default size card"
+        extra={<a href="#">More</a>}
+        style={{ width: 300 }}
+      >
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+      <Card
+        size="small"
+        title="Small size card"
+        extra={<a href="#">More</a>}
+        style={{ width: 300 }}
+      >
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+    </>
+  );
+}
+
+export default header;
