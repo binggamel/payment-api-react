7c2f740c71ab77fd4e574be04bbea4d83f607d53
binggamel <kcyeun1203@naver.com>
2021-10-02 02:33:47 +0900

    add myPage

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import productList from "./views/pList";
 import productDetail from "./views/pDetail";
 import Main from "./views/Main";
 import Login from "./views/Login";
+import MyPage from "./views/MyPage";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Header />
       <div>
         <Route path="/login" component={Login} exact={true} />
+        <Route path="/mypage" component={MyPage} exact={true} />
         <Route path="/list" component={productList} exact={true} />
         <Route path="/list/:index" component={productDetail} exact={true} />
         <Route path="/" component={Main} exact={true} />
