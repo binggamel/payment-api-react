ebdd78a0bec1790ece0f921e3107952e1838ab43
binggamel <kcyeun1203@naver.com>
2021-10-04 01:13:48 +0900

    add myPage

--- src/views/MyPage.js
+++ src/views/MyPage.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Space, Card, Button } from "antd";
 
 function MyPage() {
-  return <div>MyPage</div>;
+  return (
+    <div>
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={
+          <img src="https://pbs.twimg.com/profile_images/1193911104377778177/pK9zJgAf_400x400.jpg" />
+        }
+      >
+        <Meta title="정호비" description="www.instagram.com" />
+      </Card>
+    </div>
+  );
 }
 
 export default MyPage;
