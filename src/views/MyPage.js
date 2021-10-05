7bb7dda8cbb0715fd9c6a07f033e5c63a460abf9
binggamel <kcyeun1203@naver.com>
2021-10-05 17:54:06 +0900

    add card myPage

--- src/views/MyPage.js
+++ src/views/MyPage.js
@@ -13,6 +13,15 @@ function MyPage() {
       >
         <Meta title="정호비" description="www.instagram.com" />
       </Card>
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={
+          <img src="https://pbs.twimg.com/profile_images/1193911104377778177/pK9zJgAf_400x400.jpg" />
+        }
+      >
+        <Meta title="정호비" description="www.instagram.com" />
+      </Card>
     </div>
   );
 }
